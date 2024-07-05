e6241c383f4aa4828f3d1382180f296a75bc8c93
liliedindi <liliandindinabwire@gmail.com>
2024-07-05 09:26:31 +0300

    flow lab

---
+++ scuberGreetingForFeet.js
@@ -0,0 +1,11 @@
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  } else if (someValue <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (someValue > 2000) {
+    result = "i will gladly take your thirty bucks.";
+  }
+
+}
